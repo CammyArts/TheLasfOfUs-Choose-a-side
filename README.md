cb0393e4cd84a600166a93059a67282c533629ea
Camila Rodrigues <cammyanimatic@gmail.com>
2025-07-14 18:40:49 -0300

    mudanças simples em todo codigo

---
+++ README.md
@@ -0,0 +1,2 @@
+# TheLasfOfUs-Choose-a-side
+A website with theme of The Last Of Us I made this website in my course.
---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+function botaoUm(){
+    window.open("https://i.pinimg.com/originals/35/e5/0d/35e50dee5ebe4059557fe60e900c2663.gif");
+}
+function botaoDois(){
+    window.open("https://giffiles.alphacoders.com/219/219157.gif");
+}
